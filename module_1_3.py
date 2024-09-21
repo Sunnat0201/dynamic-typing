4f7583f9e3df57b7cf960f739aab27f400b13e89
HappY Millionaire <sunnat.fakhreev.02@gmail.com>
2024-09-21 23:44:46 +0300

    исправил ошибку

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,8 @@ name = 'Sunnat'
 print (name)
 age = 22
 print (age)
-new_age = 22 + 1
-print (new_age)
+age = 22 + 1
+print (age)
 is_student = True
 print (is_student)
-print (name, age, new_age, is_student)
+print (name, age, is_student)
